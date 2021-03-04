c67cdb6f3024ddf396a0c961569f86eabb630511
dengshuaihui <dengshuaihui@yy.com>
2021-03-04 15:31:43 +0800

    feat: fix template

--- lib/generator/vue/template/src/App.vue
+++ lib/generator/vue/template/src/App.vue
@@ -2,7 +2,6 @@
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png" />
         <HelloWorld msg="Welcome to Your Vue.js App" />
-        <h1>Welcome to Your Vue.js App</h1>
     </div>
 </template>
 
